cb267f779c5881ece5dec2b9497638082e440239
HuGuobao <398456458@qq.com>
2017-11-20 11:07:24 +0800

    新建Reflection测试项目

---
+++ src/guyue/hu/TestReflection.java
@@ -0,0 +1,14 @@
+package guyue.hu;
+/**
+*@author guyue E-mail:398456458@qq.com
+*version1.0 创建时间:2017年11月20日 上午11:06:33
+*describ 
+*/
+public class TestReflection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
