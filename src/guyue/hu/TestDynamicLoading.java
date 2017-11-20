6b3e2608ceb6e0b385c2dfc92fcb2b73001df7e1
guyuehu <398456458@qq.com>
2017-11-20 20:56:34 +0800

    删除TestReflection 增加TestDynamicLoading

---
+++ src/guyue/hu/TestDynamicLoading.java
@@ -0,0 +1,23 @@
+package guyue.hu;
+/**
+ * @author guyue
+ * @date 2017年11月20日 下午8:45:56
+ * @class describ:
+ */
+public class TestDynamicLoading {
+
+	public static void main(String[] args) {
+		new A();
+		System.out.println("---------------------------------------");
+		new B();
+	}
+
+}
+
+class A {
+	
+}
+
+class B {
+	
+}
