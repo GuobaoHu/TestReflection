0c28fffcd30897c370af939c25e5f73415d55df2
HuGuobao <398456458@qq.com>
2017-11-22 17:02:11 +0800

    测试4种ClassLoader:bootstrapCL(核心类的);extesionCL(扩展包的);applCL(用户的);其他

---
+++ src/guyue/hu/TestJDKClassLoader.java
@@ -0,0 +1,17 @@
+package guyue.hu;
+/**
+*@author guyue E-mail:398456458@qq.com
+*version1.0 创建时间:2017年11月22日 下午3:14:57
+*describ 
+*/
+public class TestJDKClassLoader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(String.class.getClassLoader());
+		System.out.println(com.sun.crypto.provider.AESKeyGenerator.class.getClassLoader().getClass().getName());
+		System.out.println(TestJDKClassLoader.class.getClassLoader().getClass().getName());
+		
+	}
+
+}
